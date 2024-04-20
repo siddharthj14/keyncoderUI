1678885eabbf1063a1d697436668de9aa5d2e744
ANKUSHUKIL03 <142995201+ANKUSHUKIL03@users.noreply.github.com>
2024-04-20 21:54:53 +0530

    Resposive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,54 @@
 import {
-	faFacebook,
-	faInstagram,
-	faLinkedin,
-	faTwitter,
-	faYoutube,
+    faFacebook,
+    faInstagram,
+    faLinkedin,
+    faTwitter,
+    faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function Footer() {
-	return (
-		<footer className='text-zinc-600 body-font mt-[30px] bottom-0'>
-			<div className='flex flex-row justify-between ml-[20px] mt-2'>
-				<p className='text-[14px]'>
-					©2024 <br />
-					keyNcoders
-				</p>
-				<span className='h-[24px] bg-[#111B47] text-white p-1 mr-[30px] flex items-center mt-2'>
-					Purchase Now
-				</span>
-			</div>
-			<hr className='w-full mt-3 bg-gray-300' />
-
-			<div className='container mx-auto py-4 px-5 flex flex-row items-center justify-between'>
-				<p className='text-black font-bold text-[26px] text-center self-center flex flex-row gap-8 items-center mr-[20px]'>
-					<img
-						src='https://placehold.co/10'
-						alt='Placeholder'
-						className=' w-[20px] h-[20px]'
-					/>
-					keyNcoders
-				</p>
-				<span className='inline-flex gap-[20px] ml-[50px] items-center self-start'>
-					<a href='#' className='text-[24px] text-[#120E0E]'>
-						About us
-					</a>
-					<a href='#' className='ml-4 text-[#120E0E] text-[24px]'>
-						Batches
-					</a>
-					<a href='#' className='ml-4 text-[#120E0E] text-[24px]'>
-						Contact Us
-					</a>
-				</span>
-				<div className='flex flex-row gap-[20px]'>
-					<FontAwesomeIcon icon={faFacebook} className='text-[#B0B8BC] h-[20px]' />
-					<FontAwesomeIcon icon={faLinkedin} className='text-[#B0B8BC] h-[20px]' />
-					<FontAwesomeIcon icon={faTwitter} className='text-[#B0B8BC] h-[20px]' />
-					<FontAwesomeIcon icon={faYoutube} className='text-[#B0B8BC] h-[20px]' />
-					<FontAwesomeIcon icon={faInstagram} className='text-[#B0B8BC] h-[20px]' />
-				</div>
-			</div>
-		</footer>
-	);
+    return (
+        <footer className='text-zinc-600 body-font mt-4 md:mt-8'>
+            <div className='container mx-auto px-5'>
+                <div className='flex flex-col md:flex-row items-center justify-between'>
+                    <p className='text-sm md:text-base text-center md:text-left'>
+                        ©2024 <br />
+                        keyNcoders
+                    </p>
+                    <span className='inline-flex mt-2 md:mt-0'>
+                        <a href='#' className='text-sm md:text-base text-[#111B47] px-3 py-1 md:px-4 md:py-2 bg-[#111B47] rounded-lg hover:bg-[#0E1840]'>
+                            Purchase Now
+                        </a>
+                    </span>
+                </div>
+                <hr className='w-full mt-3 bg-gray-300' />
+                <div className='flex flex-col md:flex-row items-center justify-between py-4'>
+                    <p className='text-black font-bold text-base md:text-lg text-center md:text-left mb-4 md:mb-0'>
+                        <img
+                            src='https://placehold.co/10'
+                            alt='Placeholder'
+                            className='w-5 h-5 md:w-6 md:h-6 mr-2 md:mr-4 inline-block'
+                        />
+                        keyNcoders
+                    </p>
+                    <span className='flex flex-col md:flex-row gap-4 md:gap-8'>
+                        <a href='#' className='text-base md:text-lg text-[#120E0E] hover:text-[#111B47]'>About us</a>
+                        <a href='#' className='text-base md:text-lg text-[#120E0E] hover:text-[#111B47]'>Batches</a>
+                        <a href='#' className='text-base md:text-lg text-[#120E0E] hover:text-[#111B47]'>Contact Us</a>
+                    </span>
+                    <div className='flex flex-row gap-4 md:gap-8'>
+                        <FontAwesomeIcon icon={faFacebook} className='text-[#B0B8BC] h-5 md:h-6' />
+                        <FontAwesomeIcon icon={faLinkedin} className='text-[#B0B8BC] h-5 md:h-6' />
+                        <FontAwesomeIcon icon={faTwitter} className='text-[#B0B8BC] h-5 md:h-6' />
+                        <FontAwesomeIcon icon={faYoutube} className='text-[#B0B8BC] h-5 md:h-6' />
+                        <FontAwesomeIcon icon={faInstagram} className='text-[#B0B8BC] h-5 md:h-6' />
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,33 +1,34 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCartShopping, faLongArrowAltDown} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faLongArrowAltDown } from "@fortawesome/free-solid-svg-icons";
+
 const Landing = () => {
-	return (
-		<section className='ml-[100px] mt-[100px]'>
-			<div className='font-sans font-bold text-[#333] text-[85px] dark:text-white'>
-				Lorem Ipsum
-				<br />
-				<span className='text-[#666] text-[70px]'>lorem ipsum</span>
-			</div>
-			<p className=' w-[700px] text-[25px] font-normal text-[#afaeae] text-wrap'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam cum
-				atque dolore, at tenetur aspernatur? Nostrum, voluptatibus harum placeat
-				tempore distinctio a, autem sint animi ipsam dolore laboriosam nobis!
-			</p>
-			<div className='mt-20 mr-6'>
-				<button className=' font-sans border-2  border-orange-400 px-4 py-2 rounded-xl  bg-orange-600 shadow-custom text-white hover:bg-orange-500 hover:text-white mr-[50px] '>
-					lorem ipsum
-				</button>
-				<button className='bg-transparent text-orange-500 border-2 border-orange-500  px-4 py-2 rounded-xl font-sans shadow-custom hover:bg-orange-500 hover:text-white'>
-					<FontAwesomeIcon icon={faCartShopping} />
-					<span className=''> Add to Cart</span>
-				</button>
-			</div>
-			<div className='mt-[100px] ml-[100px] animate-bounce text-[35px] w-[60px] h-[60px] font-light border-2 border-slate-700 dark:border-white rounded-full  text-center'>
-				<FontAwesomeIcon icon={faLongArrowAltDown} />
-			</div>
-		</section>
-	);
+    return (
+        <section className='mx-4 md:mx-0 mt-8 md:mt-16'>
+            <div className='font-sans font-bold text-[#333] text-3xl md:text-5xl dark:text-white mt-[120px]'>
+                Lorem Ipsum
+                <br />
+                <span className='text-[#666] text-2xl md:text-4xl '>lorem ipsum</span>
+            </div>
+            <p className='mt-4 md:mt-6 max-w-[700px] text-base md:text-lg font-normal text-[#afaeae]'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam cum
+                atque dolore, at tenetur aspernatur? Nostrum, voluptatibus harum placeat
+                tempore distinctio a, autem sint animi ipsam dolore laboriosam nobis!
+            </p>
+            <div className='mt-8 md:mt-12 flex flex-col md:flex-row'>
+                <button className='font-sans border-2 border-orange-400 px-4 py-2 rounded-xl bg-orange-600 shadow-custom text-white hover:bg-orange-500 hover:text-white mb-4 md:mb-0 md:mr-4'>
+                    lorem ipsum
+                </button>
+                <button className='bg-transparent text-orange-500 border-2 border-orange-500 px-4 py-2 rounded-xl font-sans shadow-custom hover:bg-orange-500 hover:text-white flex items-center'>
+                    <FontAwesomeIcon icon={faCartShopping} className='mr-2' />
+                    <span>Add to Cart</span>
+                </button>
+            </div>
+            <div className='mt-8 md:mt-12 mx-auto animate-bounce text-2xl md:text-4xl w-10 h-10 md:w-12 md:h-12 font-light border-2 border-slate-700 dark:border-white rounded-full text-center'>
+                <FontAwesomeIcon icon={faLongArrowAltDown} />
+            </div>
+        </section>
+    );
 };
 
 export default Landing;
