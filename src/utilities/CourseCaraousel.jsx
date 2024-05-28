189352cd315e914314856d215c5823dcae370ab8
Krishnanshu <krishnanshu.khanna@gmail.com>
2024-05-28 17:39:20 +0530

    chore: Update favicon and dark mode styles

--- src/utilities/CourseCaraousel.jsx
+++ src/utilities/CourseCaraousel.jsx
@@ -4,36 +4,36 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function CourseCaraousel() {
   return (
-    <div className="w-[400px] mx-auto bg-white rounded-lg shadow-lg overflow-hidden">
-      <div className="bg-[#B17373] h-48 w-[90%] mt-6 mx-auto rounded-lg"></div>
-      <div className="p-4">
-        <div className="text-sm text-zinc-500">course domain?</div>
-        <div className="text-lg font-bold mb-2 dark:text-black">
-          Course name
-        </div>
-        <div className="text-lg text-orange-500 font-bold">Price</div>
-        <hr className="my-3 border-t border-zinc-200" />
-        <div className="flex items-center justify-between text-zinc-600 text-sm">
-          <div className="flex items-center">
-            <FontAwesomeIcon icon={faClock} />
+			<div className='w-[400px] mx-auto bg-white dark:bg-[#5c5555] rounded-lg shadow-lg overflow-hidden'>
+				<div className='bg-[#B17373] h-48 w-[90%] mt-6 mx-auto rounded-lg'></div>
+				<div className='p-4'>
+					<div className='text-sm text-zinc-500 dark:text-gray-300'>
+						course domain?
+					</div>
+					<div className='text-lg font-bold mb-2 dark:text-white'>Course name</div>
+					<div className='text-lg text-orange-500 font-bold '>Price</div>
+					<hr className='my-3 border-t border-zinc-200' />
+					<div className='flex items-center justify-between text-zinc-600 text-sm'>
+						<div className='flex items-center'>
+							<FontAwesomeIcon className='dark:text-black' icon={faClock} />
 
-            <span className="ml-1">duration</span>
-          </div>
-          <div className="flex items-center">
-            <FontAwesomeIcon icon={faVideo} />
-            <span className="ml-1">no: of lect</span>
-          </div>
-          <div className="flex items-center">
-            <FontAwesomeIcon icon={faDownload} />
-            <span className="ml-1">download</span>
-          </div>
-        </div>
-        <button className="w-full bg-orange-500 text-white font-bold py-2 px-4 rounded mt-4 hover:bg-orange-600">
-          Join Course
-        </button>
-      </div>
-    </div>
-  );
+							<span className='ml-1 dark:text-white'>duration</span>
+						</div>
+						<div className='flex items-center'>
+							<FontAwesomeIcon icon={faVideo} className='dark:text-black' />
+							<span className='ml-1 dark:text-white'>no: of lect</span>
+						</div>
+						<div className='flex items-center'>
+							<FontAwesomeIcon icon={faDownload} className='dark:text-black' />
+							<span className='ml-1 dark:text-white'>download</span>
+						</div>
+					</div>
+					<button className='w-full bg-orange-500 text-white font-bold py-2 px-4 rounded mt-4 hover:bg-orange-600'>
+						Join Course
+					</button>
+				</div>
+			</div>
+		);
 }
 
 export default CourseCaraousel;
--- src/utilities/TestimonialCard.jsx
+++ src/utilities/TestimonialCard.jsx
@@ -7,8 +7,10 @@ export default function TestimonialCard({
 	testimony,
 }) {
 	return (
-		<div className='max-w-xl mx-auto p-6 mb-10 md:mb-4 bg-white shadow-lg rounded-lg'>
-			<blockquote className='italic text-zinc-600 text-lg'>{testimony}</blockquote>
+		<div className='max-w-xl mx-auto p-6 mb-10 md:mb-4 bg-white dark:bg-[#383232]  shadow-lg rounded-lg'>
+			<blockquote className='italic text-zinc-600 text-lg dark:text-gray-300'>
+				{testimony}
+			</blockquote>
 			<div className='flex items-center mt-4'>
 				<img
 					src={image}
@@ -16,14 +18,16 @@ export default function TestimonialCard({
 					className='w-20 h-16 rounded-full mr-4 border-slate-800 border-[3px]'
 				/>
 				<div>
-					<div className='ml-1 text-zinc-700'>{name}</div>
+					<div className='ml-1 text-zinc-700 dark:text-white'>{name}</div>
 					<div className='flex items-center'>
 						<img
 							src={logo}
 							alt={`${company} Logo`}
 							className='mr-2 w-10 rounded-full'
 						/>
-						<span className='text-zinc-900 font-semibold'>{company}</span>
+						<span className='text-zinc-900 dark:text-white font-semibold'>
+							{company}
+						</span>
 					</div>
 				</div>
 			</div>
