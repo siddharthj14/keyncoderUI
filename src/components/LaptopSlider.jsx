728e178d01020d56b1bd709a8c00484030f14201
Siddharth Jain <siddharthj1405@gmail.com>
2024-08-03 17:05:41 +0530

    feat : create PostLogin if use haven't buy course

---
+++ src/components/LaptopSlider.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LaptopSlider = () => {
+  return (
+    <div className="">
+            <img
+              src="../images/laptop.png"
+              alt="Laptop Slider"
+              className="max-w-xs md:max-w-lg lg:max-w-xl m-6 medium:m-2 rounded-lg "
+            />
+          </div>
+  )
+}
+
+export default LaptopSlider
